776c7260e653693a3bc27e2c59d970563c1a3816
Luca Rava <luca.rava@stud.hslu.ch>
2019-11-20 20:48:46 +0100

    add Movement to Player with Keys

---
+++ Assets/Move.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private MoveController moveController;
+
+    [SerializeField]
+    private float speed = 5f;
+
+    private bool isMoveLeft;
+    private bool isMoveRight;
+    private bool isMoveUp;
+    private bool isMoveDown;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        moveController = GetComponent<MoveController>();
+        moveController.MoveLeft += OnMoveLeft;
+        moveController.MoveRight += OnMoveRight;
+        moveController.MoveUp += OnMoveUp;
+        moveController.MoveDown += OnMoveDown;
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMoveLeft()
+    {
+        isMoveLeft = true;
+    }
+
+    private void OnMoveRight()
+    {
+        isMoveRight = true;
+    }
+
+    private void OnMoveUp()
+    {
+        isMoveUp = true;
+    }
+
+    private void OnMoveDown()
+    {
+        isMoveDown = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isMoveLeft)
+        {
+            rb.MovePosition(transform.position + Vector3.left * Time.fixedDeltaTime * speed);
+            isMoveLeft = false;
+        }
+
+        if (isMoveRight)
+        {
+            rb.MovePosition(transform.position + Vector3.right * Time.fixedDeltaTime * speed);
+            isMoveRight = false;
+        }
+
+        if (isMoveUp)
+        {
+            rb.MovePosition(transform.position + Vector3.up * Time.fixedDeltaTime * speed);
+            isMoveUp = false;
+        }
+
+        if (isMoveDown)
+        {
+            rb.MovePosition(transform.position + Vector3.down * Time.fixedDeltaTime * speed);
+            isMoveDown = false;
+        }
+    }
+
+
+    private void OnDestroy()
+    {
+        moveController.MoveLeft -= OnMoveLeft;
+        moveController.MoveRight -= OnMoveRight;
+        moveController.MoveUp -= OnMoveLeft;
+        moveController.MoveDown -= OnMoveRight;
+    }
+}
---
+++ Assets/MoveController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+
+    public event Action MoveLeft = delegate { };
+    public event Action MoveRight = delegate { };
+    public event Action MoveUp = delegate { };
+    public event Action MoveDown = delegate { };
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveLeft();
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveRight();
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveUp();
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            MoveDown();
+        }
+
+    }
+}
