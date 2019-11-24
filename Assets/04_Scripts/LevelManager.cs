6bd002ba4542d144721607360cf6bd3cc06f7da2
Luca Rava <luca.rava@stud.hslu.ch>
2019-11-24 13:49:58 +0100

    Start Game over Menu with LevelManager

---
+++ Assets/04_Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public static void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
