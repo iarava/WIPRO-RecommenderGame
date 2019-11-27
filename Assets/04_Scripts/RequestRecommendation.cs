e865a0a0cda977f680e36dad3dd8bd142836cef1
Luca Rava <luca.rava@stud.hslu.ch>
2019-11-27 16:15:04 +0100

    rename Script ShowQuestion to RequestRecommendation

--- Assets/04_Scripts/RequestRecommendation.cs
+++ Assets/04_Scripts/RequestRecommendation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShowQuestion : MonoBehaviour
+public class RequestRecommendation : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
