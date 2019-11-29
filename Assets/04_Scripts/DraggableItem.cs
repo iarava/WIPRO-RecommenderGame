781a9ee7ba4dd15c5d5f2bdef10fe399f317377d
Raphael Heer <raphael.heer@gmail.com>
2019-11-29 11:46:46 +0100

    Drag and Drop with target dropzone

--- Assets/04_Scripts/DraggableItem.cs
+++ Assets/04_Scripts/DraggableItem.cs
@@ -6,15 +6,30 @@ using UnityEngine.UI;
 
 public class DraggableItem : MonoBehaviour, IDragHandler, IDropHandler, IBeginDragHandler, IEndDragHandler
 {
-    private Canvas canvas;
+    private RecommenderItem[] canvas;
+
+    [SerializeField]
+    private Vector2 center = new Vector2(770, 510);
+    [SerializeField]
+    private float horizontalLength = 200f;
+    [SerializeField]
+    private float verticalLength = 200f;
+
 
     private void Awake()
     {
-        canvas = GetComponent<Canvas>();
+        canvas = GetComponents<RecommenderItem>();
+
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        GridLayoutGroup grid = GetComponentInParent<GridLayoutGroup>();
+        if (grid != null)
+        {
+            grid.enabled = false;
+            Debug.Log("Disabled Grid Layout");
+        }
         //canvas = Color.green;
     }
 
@@ -25,11 +40,41 @@ public class DraggableItem : MonoBehaviour, IDragHandler, IDropHandler, IBeginDr
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        GridLayoutGroup grid = GetComponentInParent<GridLayoutGroup>();
+        if (isInRecommendationField(eventData.position))
+        {
+            Debug.Log("Item placed, Grid still disabled");
+        }
+        else
+        {
+            Debug.Log("Not in the correct postion, Grid Layout enabled again");
+            grid.enabled = true;
+        }
+        Debug.Log(eventData.position.ToString());
         //canvas = Color.white;
     }
 
     public void OnDrop(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+    }
+
+    private bool isInRecommendationField(Vector2 targetPos)
+    {
+        float minX = center.x - horizontalLength / 2;
+        float maxX = center.x + horizontalLength / 2;
+        float minY = center.y - verticalLength / 2;
+        float maxY = center.y + verticalLength / 2;
+
+        Debug.Log(string.Format("X: {0} {1} ; Y: {2} {3}", minX, maxX, minY, maxY));
+        
+        if(targetPos.x > minX && targetPos.y < maxX &&
+            targetPos.y > minY && targetPos.y < maxY)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
