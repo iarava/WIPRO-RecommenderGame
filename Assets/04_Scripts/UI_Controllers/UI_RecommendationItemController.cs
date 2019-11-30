2e7cfa37df5704821252f0153478d4cc9ccfe066
Raphael Heer <raphael.heer@gmail.com>
2019-11-30 18:18:37 +0100

    Script that sets the feedback text

--- Assets/04_Scripts/UI_Controllers/UI_RecommendationItemController.cs
+++ Assets/04_Scripts/UI_Controllers/UI_RecommendationItemController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UI_RecommendationItemController : MonoBehaviour
---
+++ Assets/04_Scripts/UI_RecommendationFeedback.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_RecommendationFeedback : MonoBehaviour
+{
+    private void Awake()
+    {
+        RecommendationManager.Instance.ShowFeedback += showFeedback;
+    }
+
+    private void showFeedback(DataRecommendation recommendation)
+    {
+        GetComponentInChildren<ImageCorrectRecommendation>().gameObject.GetComponent<Image>().sprite = recommendation.ImageFeedback;
+        GetComponentInChildren<FeedbackCorrectAnswer>().gameObject.GetComponent<Text>().text = recommendation.TextFeedback;
+    }
+
+    private void OnDisable()
+    {
+        RecommendationManager.Instance.ShowFeedback -= showFeedback;
+    }
+}
