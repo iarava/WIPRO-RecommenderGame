f2d6873768932f86762bb8cbf01ad32e0a68144e
Luca Rava <luca.rava@stud.hslu.ch>
2019-11-30 18:23:06 +0100

    show and hide Recommendation

---
+++ Assets/showRecommendation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showRecommendation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        RecommendationManager.Instance.ShowRecommendation += handleShowRecommendation;
+
+        gameObject.SetActive(false);
+    }
+
+    private void handleShowRecommendation(bool active)
+    {
+        gameObject.SetActive(active);
+    }
+
+    private void OnDestroy()
+    {
+        RecommendationManager.Instance.ShowRecommendation -= handleShowRecommendation;
+    }
+}
