2197a3864355c1f2f2e386a14b0a540537e1ad27
Raphael Heer <raphael.heer@gmail.com>
2019-12-14 16:58:33 +0100

    Added credits popup to start menu

--- Assets/04_Scripts/UI_Controllers/UIStartMenuController.cs
+++ Assets/04_Scripts/UI_Controllers/UIStartMenuController.cs
@@ -1,12 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIStartMenuController : MonoBehaviour
 {
+    [SerializeField]
+    private RectTransform creditsPopup;
+    private GameObject creditsPopupObject;
+
+    private void Awake()
+    {
+        creditsPopupObject = creditsPopup.gameObject;
+        creditsPopupObject.SetActive(false);
+    }
+
     public void onStartPressed()
     {
         LevelManager.Instance.StartGame();
+    }
+
+    public void onCreditsPressed()
+    {
+        creditsPopupObject.SetActive(true);
+    }
+
+    public void onCreditsClosePressed()
+    {
+        creditsPopupObject.SetActive(false);
     }
 
     public void onQuitPressed()
