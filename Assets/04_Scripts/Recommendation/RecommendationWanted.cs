1cc959d09db277b9d1a8148681a1e8e1acbee85a
Luca Rava <luca.rava@stud.hslu.ch>
2020-01-14 21:51:55 +0100

    remove u

--- Assets/04_Scripts/Recommendation/RecommendationWanted.cs
+++ Assets/04_Scripts/Recommendation/RecommendationWanted.cs
@@ -2,15 +2,5 @@
 
 public class RecommendationWanted : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    
 }
